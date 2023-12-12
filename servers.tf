bc0f9b1c244294f24c832c18a809a9dcef13ca77
arafique458 <arafique458@gmail.com>
2023-12-12 17:51:57 -0500

    Attempted to make the code dry again. Created: data.tf to organize the data sources in the one place to make the code look good.

--- servers.tf
+++ servers.tf
@@ -1,14 +1,3 @@
-# This is done so if the owner changes the image we still have fresh image for out instances.
-data "aws_ami" "centos" {
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
-}
-
 resource "aws_instance" "instance" {
   for_each = var.components
   ami = data.aws_ami.centos.image_id
