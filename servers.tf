3b18e90f6b6b1da8780832964236de71b3397791
arafique458 <arafique458@gmail.com>
2023-12-12 18:10:08 -0500

    Added Provisiors to execute the commands remotely. in the server.tf

--- servers.tf
+++ servers.tf
@@ -7,6 +7,21 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/Arafique458/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+  }
 }
 # Aws route53 zone created
 resource "aws_route53_record" "records" {
