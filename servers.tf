24ea594e1f275556650ffea44ca02d7eddf43c7a
arafique458 <arafique458@gmail.com>
2023-12-12 17:19:21 -0500

    servers.tf state.tf file was created and defined.

--- servers.tf
+++ servers.tf
@@ -2,7 +2,7 @@
 data "aws_ami" "centos" {
   owners = ["973714476881"]
   most_recent = true
-  name_regex = "Centos-8-Devops-Practice"
+  name_regex = "Centos-8-DevOps-Practice"
 }
 
 data "aws_security_group" "allow-all" {
