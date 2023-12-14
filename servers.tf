47094e80ecdfb751b2bd7d6baa119e4485e959d7
arafique458 <arafique458@gmail.com>
2023-12-13 20:07:20 -0500

    Added Provisioner, Depends_on and for_each also fixed Host name for each server.

--- servers.tf
+++ servers.tf
@@ -1,19 +1,22 @@
 resource "aws_instance" "instance" {
-  for_each = var.components
-  ami = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = each.value["name"]
   }
+}
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  for_each = var.components
   provisioner "remote-exec" {
-
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = self.private_ip
+      host     = aws_instance.instance[each.value["name"]].private_ip
     }
     inline = [
       "rm -rf roboshop-shell",
